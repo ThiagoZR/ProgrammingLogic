f2ecfe5a8e9e3f02ae919ac07f253ce16ce79a8b
Thiago Zacarias Rufino <thiago_rufino@live.com>
2020-09-17 17:24:54 -0300

    Add Fibonacci

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("teste!")
+let term = prompt('Write how many terms do you want to see: ');
+function fib(term){
+    let preceding = 0
+    let current = 1
+    let next = 1
+
+    for(let i = 0; i < term ; i++){
+        document.write(preceding + '<br>')
+        next = current + preceding
+        preceding = current
+        current = next
+    }
+
+}
+
+console.log(fib(term))
+
+
